f1daeb6697d488567fad12f6c003fa1937a80777
Emeraudia <stevan_metayer@orange.fr>
2023-05-16 16:53:39 +0200

    Added collider

--- rdlib/src/Map.cpp
+++ rdlib/src/Map.cpp
@@ -1,6 +1,7 @@
 #include "rdlib/Map.h"
 #include "glm/fwd.hpp"
 #include "rdlib/Agent.h"
+#include "rdlib/ColliderSpriteAgent.h"
 #include "rdlib/SpriteAgent.h"
 #include <algorithm>
 #include <cstddef>
@@ -40,10 +41,20 @@ namespace rdlib {
         for(size_t y = 0; y < map.size(); y++) {
             for(size_t x = 0; x < map[y].size(); x++) {
                 if(map[y][x] == 0) continue;
-                if(x == 0 && map[y].size() -y == 0) std::cout << "0,0" << std::endl;
-                auto sprite = new SpriteAgent(data["tiles"][std::to_string(map[y][x])]["src"], vec3(x, map.size() - y, 0));
+                if(x == 0 && map[y].size()-y == 0) std::cout << "0,0" << std::endl;
+                auto sprite = new SpriteAgent(data["tiles"][std::to_string(map[y][x])]["src"], vec3(x, map.size() - y - 1, 0));
                 s_agents.push_back(sprite);
             }
+        }
+
+        // Colliders
+        for(auto coll : data["walls"]) {
+            int id = coll["id"];
+            auto colldata = data["colliders"][std::to_string(id)];
+            auto c = new ColliderSpriteAgent(
+                colldata["src"], vec2(colldata["coll"]["x"], colldata["coll"]["y"]), vec2(colldata["coll"]["w"], colldata["coll"]["h"]), 
+                vec3(coll["x"], coll["y"], 1));
+            s_agents.push_back(c);
         }
 
         
