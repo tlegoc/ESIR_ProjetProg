730bba8a995a8510d7d92292478ac842bd3821a1
tlegoc <theo@legoc.net>
2023-05-16 16:59:43 +0200

    Format map

--- rdlib/include/rdlib/Map.h
+++ rdlib/include/rdlib/Map.h
@@ -2,13 +2,20 @@
 #include <string>
 #include <list>
 
+
+#ifndef ESIR_PROJETPROG_MAP_H
+#define ESIR_PROJETPROG_MAP_H
+
 namespace rdlib {
     class Map {
     private:
-        static std::list<Agent*> s_agents;
+        static std::list<Agent *> s_agents;
 
         static void clear();
+
     public:
-        static bool load(const std::string& mapFile);
-};
+        static bool load(const std::string &mapFile);
+    };
 } // rdlib
+
+#endif
