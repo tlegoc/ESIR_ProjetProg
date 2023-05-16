e1a2be666e4fce991a72a1112762eccc805be8b8
Emeraudia <stevan_metayer@orange.fr>
2023-05-16 13:39:27 +0200

    Tilemap OK

--- rdlib/src/Map.cpp
+++ rdlib/src/Map.cpp
@@ -1,10 +1,14 @@
 #include "rdlib/Map.h"
+#include "glm/fwd.hpp"
 #include "rdlib/Agent.h"
 #include "rdlib/SpriteAgent.h"
 #include <algorithm>
+#include <cstddef>
 #include <fstream>
 #include "json.hpp"
 #include <iostream>
+#include <string>
+#include <vector>
 
 namespace rdlib {
     std::list<Agent*> Map::s_agents;
@@ -23,19 +27,26 @@ namespace rdlib {
         std::ifstream f(mapFile);
         
         // Check if file is opened
-        if (!f.is_open())
+        if (!f.is_open()) {
+            std::cout << "Failed to open file (" << mapFile << ")" << std::endl;
             return false;
+        }
         
         // Parse
         nlohmann::json data = nlohmann::json::parse(f);
         std::cout << data["background"]["image"] << std::endl;
 
-        
-
-        int x = data["background"]["x"];
-        int y = data["background"]["y"];
-
-        std::cout << x << " " << y << std::endl;
+        // Map
+        std::vector<std::vector<int>> map = data["map"];
+        std::cout << map[0].size() << std::endl;
+        for(size_t y = 0; y < map.size(); y++) {
+            for(size_t x = 0; x < map[y].size(); x++) {
+                if(map[y][x] == 0) continue;
+                if(x == 0 && map[y].size() -y == 0) std::cout << "0,0" << std::endl;
+                auto sprite = new SpriteAgent(data["tiles"][std::to_string(map[y][x])]["src"], vec3(x, map.size() - y, 0));
+                s_agents.push_back(sprite);
+            }
+        }
         return true;   
     }
 } // rdlib
